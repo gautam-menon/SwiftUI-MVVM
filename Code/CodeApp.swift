3eb8ead7648204ece2dae8f636e0b2a240d35aa0
Gautam <gautam.menon01@gmail.com>
2023-07-06 19:16:07 +0530

    Initial Commit

---
+++ Code/CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeApp.swift
+//  Code
+//
+//  Created by Gautam Menon on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
